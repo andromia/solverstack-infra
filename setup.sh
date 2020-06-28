787806523cff11f65f23309c0d849b4ff92a469e
Phillip Rognerud <philliprognerud2@yahoo.com>
2020-06-28 01:08:21 -0700

    updated docker compose and setup script

--- setup.sh
+++ setup.sh
@@ -1,44 +1,33 @@
 #!/bin/bash
 
-# Clear old setups
-mv Dockerfiles/client/ Dockerfiles/client-delete/
-mkdir Dockerfiles/client/
+# Clean up
+cd Dockerfiles
+mv client/Dockerfile client/.dockerignore . && rm -rf client/*
+mv Dockerfile .dockerignore client/
 
-# Keep Dockerfile and dockerignore
-mv Dockerfiles/client-delete/Dockerfile Dockerfiles/client/
-mv Dockerfiles/client-delete/.dockerignore Dockerfiles/client/
+cd Dockerfiles
+mv user-auth/Dockerfile user-auth/.dockerignore . && rm -rf user-auth/*
+mv Dockerfile .dockerignore user-auth/
 
-rm -rf Dockerfiles/client-delete/
-
-mv Dockerfiles/user-auth/ Dockerfiles/user-auth-delete/
-mkdir Dockerfiles/user-auth/
-
-# Keep Dockerfile and dockerignore
-mv Dockerfiles/user-auth-delete/Dockerfile Dockerfiles/user-auth/
-mv Dockerfiles/user-auth-delete/.dockerignore Dockerfiles/user-auth/
-
-rm -rf Dockerfiles/user-auth-delete/
-
-mv Dockerfiles/user-crud/ Dockerfiles/user-crud-delete/
-mkdir Dockerfiles/user-crud/
-
-# Keep Dockerfile and dockerignore
-mv Dockerfiles/user-crud-delete/Dockerfile Dockerfiles/user-crud/
-mv Dockerfiles/user-crud-delete/.dockerignore Dockerfiles/user-crud/
-
-rm -rf Dockerfiles/user-crud-delete/
+cd Dockerfiles
+mv user-crud/Dockerfile user-crud/.dockerignore . && rm -rf user-crud/*
+mv Dockerfile .dockerignore user-crud/
 
 # Clone Microservices
+cd client/
 git clone git@github.com:andromia/cvrp-client.git
-git clone git@github.com:andromia/cvrp-user-auth.git
-git clone git@github.com:andromia/cvrp-user-crud.git
-
-# Move projects into correct directorys
-cp -r cvrp-client/* Dockerfiles/client/
-cp -r cvrp-user-auth/* Dockerfiles/user-auth/
-cp -r cvrp-user-crud/* Dockerfiles/user-crud/
-
-# Clean up git clones
+cp -r cvrp-client/* .
 rm -rf cvrp-client
+cd ..
+
+cd user-auth/
+git clone git@github.com:andromia/cvrp-user-auth.git
+cp -r cvrp-user-auth/* .
 rm -rf cvrp-user-auth
+cd ..
+
+cd user-crud/
+git clone git@github.com:andromia/cvrp-user-crud.git
+cp -r cvrp-user-crud/* .
 rm -rf cvrp-user-crud
+cd ..
