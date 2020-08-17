c30e50cea8a277b077f9044b9c9192f9409ec95a
chris pryer <christophpryer@gmail.com>
2020-08-17 12:21:50 -0400

    updated infra names

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 # Clean up
 cd Dockerfiles
-mv web-client/Dockerfile web-client/.dockerignore . && rm -rf web-client/*
-mv Dockerfile .dockerignore web-client/
+mv client/Dockerfile client/.dockerignore . && rm -rf client/*
+mv Dockerfile .dockerignore client/
 
 cd Dockerfiles
 mv user-auth/Dockerfile user-auth/.dockerignore . && rm -rf user-auth/*
@@ -14,14 +14,14 @@ mv user-crud/Dockerfile user-crud/.dockerignore . && rm -rf user-crud/*
 mv Dockerfile .dockerignore user-crud/
 
 cd Dockerfiles
-mv vrp-rpc/Dockerfile vrp-rpc/.dockerignore . && rm -rf vrp-rpc/*
-mv Dockerfile .dockerignore vrp-rpc/
+mv route/Dockerfile route/.dockerignore . && rm -rf route/*
+mv Dockerfile .dockerignore route/
 
 # Clone Microservices
-cd web-client/
-git clone git@github.com:andromia/solverstack-web-client.git
-cp -r solverstack-web-client/* .
-rm -rf solverstack-web-client
+cd client/
+git clone git@github.com:andromia/solverstack-client.git
+cp -r solverstack-client/* .
+rm -rf solverstack-client
 cd ..
 
 cd user-auth/
@@ -36,8 +36,8 @@ cp -r solverstack-user-crud/* .
 rm -rf solverstack-user-crud
 cd ..
 
-cd vrp-rpc/
-git clone git@github.com:andromia/solverstack-vrp-rpc.git
-cp -r solverstack-vrp-rpc/* .
-rm -rf solverstack-vrp-rpc
+cd route/
+git clone git@github.com:andromia/solverstack-route.git
+cp -r solverstack-route/* .
+rm -rf solverstack-route
 cd ..
